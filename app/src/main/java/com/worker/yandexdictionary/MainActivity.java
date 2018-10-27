d2626bb632cf4c60c57290522ea1f6554f586025
hataketsu <hataketsu@gmail.com>
2018-10-27 11:23:33 +0700

    capture text from image

--- app/src/main/java/com/worker/yandexdictionary/MainActivity.java
+++ app/src/main/java/com/worker/yandexdictionary/MainActivity.java
@@ -2,11 +2,12 @@ package com.worker.yandexdictionary;
 
 import android.Manifest;
 import android.app.ProgressDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,11 +15,18 @@ import android.widget.Toast;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.github.kayvannj.permission_utils.Func;
 import com.github.kayvannj.permission_utils.PermissionUtil;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.vansuita.pickimage.bean.PickResult;
+import com.vansuita.pickimage.bundle.PickSetup;
+import com.vansuita.pickimage.dialog.PickImageDialog;
+import com.vansuita.pickimage.listeners.IPickResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,7 +34,6 @@ import org.json.JSONObject;
 
 import java.net.URLEncoder;
 import java.text.MessageFormat;
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     private ProgressDialog waitDialog;
     private Spinner toSpn;
     private Spinner fromSpn;
+    private ImageButton inputCaptureBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViews() {
         translateBtn = findViewById(R.id.translateBtn);
+        inputCaptureBtn = findViewById(R.id.inputCaptureBtn);
         inputED = findViewById(R.id.inputED);
         outputTV = findViewById(R.id.outputTV);
         fromSpn = findViewById(R.id.fromSpn);
@@ -67,10 +76,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String inputText = inputED.getText().toString().trim();
                 inputText = URLEncoder.encode(inputText);
-                String fromLanguge = LanguageHelper.LANGUAGES_CODE[fromSpn.getSelectedItemPosition()];
-                String toLanguge = LanguageHelper.LANGUAGES_CODE[toSpn.getSelectedItemPosition()];
+                String fromLanguage = LanguageHelper.LANGUAGES_CODE[fromSpn.getSelectedItemPosition()];
+                String toLanguage = LanguageHelper.LANGUAGES_CODE[toSpn.getSelectedItemPosition()];
                 waitDialog.show();
-                queue.add(new StringRequest(MessageFormat.format(BASE_URL, API_KEY, inputText, fromLanguge + "-" + toLanguge), new Response.Listener<String>() {
+                queue.add(new StringRequest(MessageFormat.format(BASE_URL, API_KEY, inputText, fromLanguage + "-" + toLanguage), new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         waitDialog.dismiss();
@@ -78,9 +87,9 @@ public class MainActivity extends AppCompatActivity {
                             JSONObject result = new JSONObject(response);
                             String language = result.getString("lang");
                             String fromLanguge = language.split("-")[0];
-                            String toLanguge = language.split("-")[1];
+                            String toLanguage = language.split("-")[1];
                             fromSpn.setSelection(LanguageHelper.getIndex(fromLanguge));
-                            toSpn.setSelection(LanguageHelper.getIndex(toLanguge));
+                            toSpn.setSelection(LanguageHelper.getIndex(toLanguage));
                             JSONArray lines = result.getJSONArray("text");
                             StringBuilder buf = new StringBuilder();
                             for (int i = 0; i < lines.length(); i++) {
@@ -99,6 +108,35 @@ public class MainActivity extends AppCompatActivity {
                         toast(error.getMessage());
                     }
                 }));
+            }
+        });
+
+        inputCaptureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PickImageDialog.build(new PickSetup()).setOnPickResult(new IPickResult() {
+                    @Override
+                    public void onPickResult(PickResult pickResult) {
+                        Throwable error = pickResult.getError();
+                        if (error == null) {
+                            FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(pickResult.getBitmap());
+                            FirebaseVision.getInstance().getOnDeviceTextRecognizer().processImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                                @Override
+                                public void onSuccess(FirebaseVisionText result) {
+                                    StringBuilder buf = new StringBuilder();
+                                    for (FirebaseVisionText.TextBlock block : result.getTextBlocks()) {
+                                        for (FirebaseVisionText.Line line : block.getLines())
+                                            buf.append(line.getText()).append("\n");
+                                    }
+                                    inputED.setText(buf);
+                                }
+                            });
+                        } else {
+                            toast(error.getMessage());
+                        }
+
+                    }
+                }).show(MainActivity.this);
             }
         });
 
